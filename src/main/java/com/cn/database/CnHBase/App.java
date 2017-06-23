2956e2501327151aef171aa1552596a35ff667f9
cloudera <cloudera@quickstart.cloudera>
2017-06-22 17:39:34 -0700

    Added basic files

---
+++ src/main/java/com/cn/database/CnHBase/App.java
@@ -0,0 +1,13 @@
+package com.cn.database.CnHBase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
